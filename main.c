0e423dddd0632940bb03f901f84e9c0d0967eb36
Sam <ssohaib@gmail.com>
2025-04-15 18:03:28 +0300

    Version2

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
 this is thrid version of the proj1
 now this main.c is inside .gitignore
 lets make changes in intz
-Hello from the other side!!!!!!!!!!!!!!!!!!
+Hello from the other side!!!!!!!!!!!!!!!!!!!
